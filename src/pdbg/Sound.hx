1e13b37591cd9bdce667f46267730ea6c1a12ecf
kubohisa <kubohisa@gmail.com>
2021-01-15 06:32:59 +0900

    Update Sound.hx

--- src/pdbg/Sound.hx
+++ src/pdbg/Sound.hx
@@ -1,19 +1,25 @@
 package pdbg;
 
 class Sound {
-	
+
 	//　手探りで製作中
 
-	static var musicRes: hxd.res.Sound = null;
 	static var musicChannel: hxd.snd.Channel;
 	static var musicVol: Int = 100;
 
 	public static function musicPlay(file: String) {
-		musicRes = hxd.Res.loader.load("musics/" + file).toSound();
+		var musicRes: hxd.res.Sound = hxd.Res.loader.load("musics/" + file + ".ogg").toSound();
 
 		if(musicRes != null){
-			//Play the music and loop it
 			musicChannel = musicRes.play(true, musicVol / 100);
+		}
+	}
+
+	public static function musicQueue(file: String) {
+		var sound: hxd.res.Sound = hxd.Res.loader.load("musics/" + file + ".ogg").toSound();
+
+		if(sound != null){
+			musicChannel.queueSound(sound);
 		}
 	}
 
@@ -44,6 +50,7 @@ class Sound {
 	{
 		if (volume >= 0 && volume <= 100) {
 			musicVol = volume;
+
 			if (musicChannel != null) {
 				musicChannel.volume = musicVol / 100;
 			}
