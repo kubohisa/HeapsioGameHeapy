64b10375e236d08fabbb22cdd14796042a1d0b49
kubohisa <kubohisa@gmail.com>
2021-01-15 17:08:58 +0900

    Update Sound.hx

--- src/pdbg/Sound.hx
+++ src/pdbg/Sound.hx
@@ -4,14 +4,43 @@ class Sound {
 
 	//　手探りで製作中
 
+	static var manager: hxd.snd.Manager;
+
+	// init.
+	public static function init() {
+		manager = hxd.snd.Manager.get();
+		masterVolume(masterVol);
+		manager.masterSoundGroup.maxAudible = 6;
+	}
+
+	public static function end() {
+		manager.stopAll();
+		manager.cleanCache();
+		seVol = [];
+		se = [];
+	}
+
+	// Master.
+	static var masterVol: Int = 100;
+
+	public static function masterVolume(volume: Int)
+	{
+		if (volume >= 0 && volume <= 100) {
+			masterVol = volume;
+
+			manager.masterVolume = masterVol / 100;
+		}
+	}
+
+	// Back ground Mauic.
 	static var musicChannel: hxd.snd.Channel;
 	static var musicVol: Int = 100;
 
 	public static function musicPlay(file: String) {
-		var musicRes: hxd.res.Sound = hxd.Res.loader.load("musics/" + file + ".ogg").toSound();
+		var sound: hxd.res.Sound = hxd.Res.loader.load("musics/" + file + ".ogg").toSound();
 
-		if(musicRes != null){
-			musicChannel = musicRes.play(true, musicVol / 100);
+		if(sound != null){
+			musicChannel = sound.play(true, musicVol / 100);
 		}
 	}
 
@@ -54,6 +83,44 @@ class Sound {
 			if (musicChannel != null) {
 				musicChannel.volume = musicVol / 100;
 			}
+		}
+	}
+
+	// SE.
+	// 効果音のデータが無いためデバッグは出来てない
+	static var se: Map<String, hxd.snd.Channel> = [];
+	static var seVol: Map<String, Int>;
+
+	public static function sePlay(name: String, file: String) {
+		var sound: hxd.res.Sound = hxd.Res.loader.load("se/" + file + ".ogg").toSound();
+
+		if (seVol[name] == null) seVol[name] = 100;
+
+		if(sound != null){
+			se[name] = sound.play(true, seVol[name] / 100);
+		}
+	}
+
+	public static function sePause(name: String)
+	{
+		if (musicChannel != null) {
+			se[name].pause = !se[name].pause;
+		}
+	}
+
+	public static function seStop(name: String)
+	{
+		if (se[name] != null) {
+			se[name].stop();
+		}
+	}
+
+	public static function seVolume(name: String, volume: Int)
+	{
+		if (volume >= 0 && volume <= 100) {
+			seVol[name] = volume;
+
+			se[name].volume = seVol[name] / 100;
 		}
 	}
 }
