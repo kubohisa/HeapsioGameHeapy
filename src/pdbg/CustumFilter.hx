00e3b08117f99d834704e1c1195df718e7413b32
kubohisa <kubohisa@gmail.com>
2021-02-02 08:08:39 +0900

    Update CustumFilter.hx

--- src/pdbg/CustumFilter.hx
+++ src/pdbg/CustumFilter.hx
@@ -19,6 +19,10 @@ class CustumFilter {
 	public static function colorMonochrome() {
 		return new h2d.filter.Shader(new PdbgFilterMonochrome());
 	}
+
+	public static function colorEdge() {
+		return new h2d.filter.Shader(new PdbgFilterEdge());
+	}
 }
 
 class PdbgFilterInversion extends h3d.shader.ScreenShader {
@@ -45,6 +49,39 @@ class PdbgFilterMonochrome extends h3d.shader.ScreenShader {
 			pixelColor.r = c;
 			pixelColor.g = c;
 			pixelColor.b = c;
+		}
+	}
+}
+
+class PdbgFilterEdge extends h3d.shader.ScreenShader {
+	static var SRC = {
+		@param var texture:Sampler2D;
+		@param var width:Float;
+		function fragment() {
+			pixelColor = texture.get(input.uv);
+			var pc = (pixelColor.r + pixelColor.g + pixelColor.b) / 3;
+
+			var tf = 1 / width;
+			var count = 0;
+			var flag = 0;
+			var ac:Float = 0.0;
+			for (y in -1...1) {
+				for (x in -1...1) {
+					if (tf * x < 0 || tf * x > 1)
+						continue;
+					if (tf * y < 0 || tf * y > 1)
+						continue;
+
+					var a = texture.get(input.uv + vec2(tf * x, tf * y));
+					var c = (a.r + a.g + a.b) / 3;
+
+					if (abs(pc - c) > 0.6) {
+						ac = 1.0;
+					}
+				}
+			}
+
+			pixelColor.rgb = vec3(ac);
 		}
 	}
 }
